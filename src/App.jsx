c4485710eef63fb09870ef7b436b4dc33a88900e
Lyrecoph <lyrecoph30@gmail.com>
2024-08-12 14:43:08 +0100

    Clearing Tasks & Fixing Minor Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       projectId: prevState.selectedProjectId,
       id: taskId,
       };
-      
+
       return {
         ...prevState,
         tasks: [...prevState.tasks, newTask]
@@ -29,7 +29,15 @@ function App() {
     })
   }
 
-  function handleDeleteTask(){}
+  function handleDeleteTask(id){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter(task => 
+          task.id !== id)
+      }
+    })
+  }
 
 
   function handleSelectProject(id){
@@ -115,6 +123,7 @@ function App() {
         onStartAddProject={handleStartAddProject} 
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
